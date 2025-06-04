fa38a87d7e451c5f14cf4da7e1366a9a5e008e4e
Nobody Nose <nobodyn0se@protonmail.ch>
2025-06-04 08:40:51 +0530

    Added SQL queries for DB interactions

---
+++ src/queries/airports-queries.js
@@ -0,0 +1,48 @@
+// prettier-ignore
+
+// queries.js
+
+const createAirportsTableQuery = `
+  CREATE TABLE IF NOT EXISTS airports
+  (
+    id INTEGER PRIMARY KEY,
+    ident VARCHAR(4),
+    type TEXT,
+    name TEXT NOT NULL,
+    lat NUMERIC NOT NULL,
+    long NUMERIC NOT NULL,
+    elevation INTEGER,
+    icao VARCHAR(4) NOT NULL,
+    iata VARCHAR(3) NOT NULL,
+    country VARCHAR(4)
+  );
+`;
+
+const selectAllAirportsQuery = `
+  SELECT * FROM airports;
+`;
+
+
+const insertAirportQuery = (columns, values) => `
+  INSERT INTO airports (${columns})
+  VALUES (${values}) ON CONFLICT (id) DO NOTHING;
+`;
+
+const updateAirportQuery = (updateConditions, id) => `
+  UPDATE airports
+  SET ${updateConditions}
+  WHERE id = $${id};
+`;
+
+const insertAirportsBatchQuery = (columns, values) => `
+  INSERT INTO airports (${columns})
+  VALUES ${values} ON CONFLICT (id) DO NOTHING;
+`;
+
+module.exports = {
+  createAirportsTableQuery,
+  selectAllAirportsQuery,
+  insertAirportQuery,
+  updateAirportQuery,
+  insertAirportsBatchQuery,
+};
