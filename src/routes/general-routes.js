28199a37e2716d9f75cf71f07e0acdff58c3cfad
Nobody Nose <nobodyn0se@protonmail.ch>
2025-06-13 16:00:30 +0530

    Defined route for airport search

--- src/routes/general-routes.js
+++ src/routes/general-routes.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const logger = require('../middleware/logger');
 const { createTestError } = require('../util/util');
+const { searchAirportByUser } = require('../services/db');
+
+router.get('/get/search', (req, res, next) => {
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    searchAirportByUser(searchTerm)
+      .then((result) => {
+        logger.info('/search route returned results successfully');
+        res.status(200).json(result);
+      })
+      .catch(next);
+  }
+});
 
 router.get('/error/:statusCode', (req, res, next) => {
   if (req.ip === '::ffff:127.0.0.1') {
     next(createTestError(req.params.statusCode));
   } else {
-    const error = new Error(
-      'Denied error route access to an outsider'
-    );
+    const error = new Error('Denied error route access to an outsider');
     error.statusCode = 403;
     next(error);
   }
