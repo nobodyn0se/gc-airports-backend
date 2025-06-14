24265339693096285bbd31c1658592fbff12b4b9
Nobody Nose <nobodyn0se@protonmail.ch>
2025-06-14 13:27:37 +0530

    Added a route validator for search query

--- src/routes/general-routes.js
+++ src/routes/general-routes.js
@@ -6,14 +6,18 @@ const db = require('../services/db');
 
 router.get('/get/search', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
-  if (searchTerm) {
-    db.searchAirportByUser(searchTerm)
-      .then((result) => {
-        logger.info('/search route returned results successfully');
-        res.status(200).json(result);
-      })
-      .catch(next);
+  if (!searchTerm) {
+    const error = new Error('Search term is missing');
+    error.status = 400;
+    return next(error);
   }
+
+  db.searchAirportByUser(searchTerm)
+    .then((result) => {
+      logger.info('/search route returned results successfully');
+      res.status(200).json(result);
+    })
+    .catch(next);
 });
 
 router.get('/error/:statusCode', (req, res, next) => {
