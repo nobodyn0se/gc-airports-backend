02ece165af4e699872505ccc308d90da59ad5b3e
Nobody Nose <nobodyn0se@protonmail.ch>
2025-06-14 12:37:41 +0530

    Updated route handler for testability

--- src/routes/general-routes.js
+++ src/routes/general-routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../middleware/logger');
 const { createTestError } = require('../util/util');
-const { searchAirportByUser } = require('../services/db');
+const db = require('../services/db');
 
 router.get('/get/search', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
   if (searchTerm) {
-    searchAirportByUser(searchTerm)
+    db.searchAirportByUser(searchTerm)
       .then((result) => {
         logger.info('/search route returned results successfully');
         res.status(200).json(result);
